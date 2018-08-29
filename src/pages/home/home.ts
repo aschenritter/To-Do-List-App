17138906c234f28b96226b1d6a15bd04a36355b5
Schwarzlicht <dgerozaga@gmail.com>
2018-08-30 00:12:01 +0800

    complete crud

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { DataProvider } from '../../providers/data/data';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+	todoList: string[] = [];
+    todo: string;
+	
+  constructor(public navCtrl: NavController, public dataService: DataProvider) {
+	this.dataService.getData().then((todos) => {
+		if(todos) {
+			this.todoList = todos;
+		}
+	});
   }
 
+	add() {
+        this.todoList.push(this.todo);
+		this.dataService.save(this.todoList);
+        this.todo = "";
+    }
+	
+	edit(item) {
+		var index = this.todoList.indexOf(item, 0);
+        if (index > -1 && this.todo != "") {
+            this.todoList[index] = this.todo;
+			this.dataService.save(this.todoList);
+			this.todo = "";
+        }
+	}
+	
+    delete(item) {
+        var index = this.todoList.indexOf(item, 0);
+        if (index > -1) {
+            this.todoList.splice(index, 1);
+			this.dataService.save(this.todoList);
+        }
+    }
 }
